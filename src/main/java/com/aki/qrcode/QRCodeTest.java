4ebe596bd2685d9360ee76f02303d216c64c40b7
kamiaki <247557768@qq.com>
2019-12-28 17:07:55 +0800

    成了

--- src/main/java/com/aki/qrcode/QRCodeTest.java
+++ src/main/java/com/aki/qrcode/QRCodeTest.java
@@ -1,9 +1,13 @@
 package com.aki.qrcode;
 
-import java.io.File;
+import java.awt.image.BufferedImage;
 
 public class QRCodeTest {
     public static void main(String[] args) {
-
+        try {
+            QrCodeUtils.encode("adsadsdsa",null,"d:/",false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/aki/qrcode/QrCodeUtils.java
+++ src/main/java/com/aki/qrcode/QrCodeUtils.java
@@ -10,10 +10,12 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Hashtable;
+import java.util.Random;
 
 /**
  * 二维码生成工具类
@@ -38,7 +40,7 @@ public class QrCodeUtils {
      * @throws Exception
      */
     public static BufferedImage createImage(String content, String logoPath, boolean needCompress) throws Exception {
-        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<>();
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
         hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
         hints.put(EncodeHintType.MARGIN, 1);
@@ -71,7 +73,7 @@ public class QrCodeUtils {
     private static void insertImage(BufferedImage source, String logoPath, boolean needCompress) throws IOException {
         InputStream inputStream = null;
         try {
-            inputStream = QrCodeUtils.getResourceAsStream(logoPath);
+            inputStream = FileUtils.getResourceAsStream(logoPath);
             Image src = ImageIO.read(inputStream);
             int width = src.getWidth(null);
             int height = src.getHeight(null);
@@ -110,6 +112,55 @@ public class QrCodeUtils {
 
     /**
      * 生成二维码(内嵌LOGO)
+     * 二维码文件名随机，文件名可能会有重复
+     *
+     * @param content      内容
+     * @param logoPath     LOGO地址
+     * @param destPath     存放目录
+     * @param needCompress 是否压缩LOGO
+     * @throws Exception
+     */
+    public static String encode(String content, String logoPath, String destPath, boolean needCompress) throws Exception {
+        BufferedImage image = QrCodeUtils.createImage(content, logoPath, needCompress);
+        mkdirs(destPath);
+        String fileName = new Random().nextInt(99999999) + "." + FORMAT.toLowerCase();
+        ImageIO.write(image, FORMAT, new File(destPath + "/" + fileName));
+        return fileName;
+    }
+
+    /**
+     * 生成二维码(内嵌LOGO)
+     * 调用者指定二维码文件名
+     *
+     * @param content      内容
+     * @param logoPath     LOGO地址
+     * @param destPath     存放目录
+     * @param fileName     二维码文件名
+     * @param needCompress 是否压缩LOGO
+     * @throws Exception
+     */
+    public static String encode(String content, String logoPath, String destPath, String fileName, boolean needCompress) throws Exception {
+        BufferedImage image = QrCodeUtils.createImage(content, logoPath, needCompress);
+        mkdirs(destPath);
+        fileName = fileName.substring(0, fileName.indexOf(".") > 0 ? fileName.indexOf(".") : fileName.length())
+                + "." + FORMAT.toLowerCase();
+        ImageIO.write(image, FORMAT, new File(destPath + "/" + fileName));
+        return fileName;
+    }
+
+    /**
+     * 生成二维码
+     *
+     * @param content  内容
+     * @param destPath 存储地址
+     * @throws Exception
+     */
+    public static String encode(String content, String destPath) throws Exception {
+        return QrCodeUtils.encode(content, null, destPath, false);
+    }
+
+    /**
+     * 生成二维码 输出流(内嵌LOGO)
      *
      * @param content      内容
      * @param logoPath     LOGO地址
@@ -124,12 +175,26 @@ public class QrCodeUtils {
     }
 
     /**
-     * 获取指定文件的输入流，获取logo
+     * 生成二维码 输出流
      *
-     * @param logoPath 文件的路径
-     * @return
+     * @param content 内容
+     * @param output  输出流
+     * @throws Exception
      */
-    public static InputStream getResourceAsStream(String logoPath) {
-        return QrCodeUtils.class.getResourceAsStream(logoPath);
+    public static void encode(String content, OutputStream output) throws Exception {
+        QrCodeUtils.encode(content, null, output, false);
+    }
+
+    /**
+     * 当文件夹不存在时，mkdirs会自动创建多层目录，区别于mkdir．
+     * (mkdir如果父目录不存在则会抛出异常)
+     *
+     * @param destPath 存放目录
+     */
+    public static void mkdirs(String destPath) {
+        File file = new File(destPath);
+        if (!file.exists() && !file.isDirectory()) {
+            file.mkdirs();
+        }
     }
 }
