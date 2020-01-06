0f91499a2e4560d92ff485f4ae5d70645e79c61a
kamiaki <247557768@qq.com>
2020-01-06 10:38:34 +0800

    输入 输出

--- src/main/java/com/aki/qrcode/QRCodeController.java
+++ src/main/java/com/aki/qrcode/QRCodeController.java
@@ -3,9 +3,7 @@ package com.aki.qrcode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.OutputStream;
 
 /**
  * 二维码生成类
@@ -14,14 +12,10 @@ import java.io.OutputStream;
 public class QRCodeController {
 
     @RequestMapping("/createQrCode")
-    public void createQrCode(HttpServletRequest request, HttpServletResponse response) {
-        StringBuffer url = request.getRequestURL();
+    public void createQrCode(String msg, HttpServletResponse response) {
+        if (msg == null || msg.isEmpty()) msg = "啥也没写";
         try {
-            OutputStream os = response.getOutputStream();
-            //从配置文件读取需要生成二维码的连接
-//            String requestUrl = GraphUtils.getProperties("requestUrl");
-            //requestUrl:需要生成二维码的连接，logoPath：内嵌图片的路径，os：响应输出流，needCompress:是否压缩内嵌的图片
-            QrCodeUtils.encode("一段测试文字", null, os, true);
+            QrCodeUtils.encode(msg, null, response.getOutputStream(), true);
         } catch (Exception e) {
             e.printStackTrace();
         }
